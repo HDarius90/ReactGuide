2230ab1b437f59885779244c8f5897ad8dfd8b9e
Darius Hadjati <darius.hadjati@gmail.com>
2023-09-03 19:02:38 +0200

    creating card component and organizing component files

--- src/components/Expense/ExpenseItem.jsx
+++ src/components/Expense/ExpenseItem.jsx
@@ -1,14 +1,15 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 export default function ExpenseItem({ expenses }) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={expenses.date} />
       <div className="expense-item__description">
         <h2>{expenses.title}</h2>
         <div className="expense-item__price">${expenses.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
--- src/components/Expense/Expenses.jsx
+++ src/components/Expense/Expenses.jsx
@@ -1,13 +1,14 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 
 export default function Expenses({expenses}) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem expenses={expenses[0]} />
       <ExpenseItem expenses={expenses[1]} />
       <ExpenseItem expenses={expenses[2]} />
       <ExpenseItem expenses={expenses[3]} />
-    </div>
+    </Card>
   );
 }
