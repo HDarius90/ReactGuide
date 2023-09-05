c8dfba06f134aacbf608986218950a22c2d193ec
Darius Hadjati <darius.hadjati@gmail.com>
2023-09-05 21:26:46 +0200

    Working with Multiple States

--- react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
@@ -1,9 +1,34 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
   };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitClickHandler = (event) => {
+    event.preventDefault();
+    const newExpense = {
+      id: "e5",
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate,
+    };
+    console.log(newExpense);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -13,15 +38,27 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <button type="submit" onClick={submitClickHandler}>
+          Add Expense
+        </button>
       </div>
     </form>
   );
