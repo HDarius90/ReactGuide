1c951e823d8256535085d2737bdcaf2e71c3e0f0
Darius Hadjati <darius.hadjati@gmail.com>
2023-09-05 15:47:15 +0200

    Working with State

--- react-complete-guide/src/components/Expense/ExpenseItem.jsx
+++ react-complete-guide/src/components/Expense/ExpenseItem.jsx
@@ -1,15 +1,26 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
-export default function ExpenseItem({ expenses }) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={expenses.date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenses.title}</h2>
-        <div className="expense-item__price">${expenses.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
-}
+};
+
+export default ExpenseItem;
