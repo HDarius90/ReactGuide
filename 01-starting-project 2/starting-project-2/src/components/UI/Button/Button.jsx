0d0ad25d5762b9d1ad0082959fe2707cb0ac516b
Darius Hadjati <darius.hadjati@gmail.com>
2023-09-11 21:26:23 +0200

    Using CSS Modules

--- 01-starting-project 2/starting-project-2/src/components/UI/Button/Button.jsx
+++ 01-starting-project 2/starting-project-2/src/components/UI/Button/Button.jsx
@@ -1,7 +1,9 @@
-import "./Button.css";
+import styles from "./Button.module.css";
+
+/*
 import styled from "styled-components";
 
-const Button = styled.button`
+ const Button = styled.button`
 width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
@@ -25,14 +27,14 @@ width: 100%;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`;
+`; */
 
-/* const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
-}; */
+};
 
 export default Button;
