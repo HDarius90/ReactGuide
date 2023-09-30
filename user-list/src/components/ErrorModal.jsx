86b344c6c70cf990560637d73c5b1fc1080c5fd4
Darius Hadjati <darius.hadjati@gmail.com>
2023-09-30 19:29:32 +0200

    Working with Portals

--- user-list/src/components/ErrorModal.jsx
+++ user-list/src/components/ErrorModal.jsx
@@ -1,18 +1,36 @@
 import style from "./ErrorModal.module.css";
 import Button from "./Button";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={style.backdrop}>{props.children}</div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={style.modal}>
+      <div className={style.header}>
+        <h2>Invalid input</h2>
+      </div>
+      <div className={style.content}>{props.msg}</div>
+      <div className={style.actions}>
+        <Button clickHandler={props.onDismiss}>Okay</Button>
+      </div>
+    </div>
+  );
+};
 
 export default function ErrorModal({ msg, onDismiss }) {
   return (
-    <div className={style.backdrop}>
-      <div className={style.modal}>
-        <div className={style.header}>
-          <h2>Invalid input</h2>
-        </div>
-        <div className={style.content}>{msg}</div>
-        <div className={style.actions}>
-          <Button clickHandler={onDismiss}>Okay</Button>
-        </div>
-      </div>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay msg={msg} onDismiss={onDismiss} />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 }
